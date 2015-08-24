f6ed3a687e4c37770002469ca9400bae952e4ac7
marius <mmillea@ucdavis.edu>
2015-08-24 01:01:53 -0500

    thin ticker output and chaged progress flie

--- ticker.f90
+++ ticker.f90
@@ -8,12 +8,13 @@ module ticker
 
 
     integer, parameter :: NTICKS_MAX=1048576
+    integer, parameter :: THIN_OUTPUT=100
     real, dimension(1:NTICKS_MAX) :: tick_time
     integer :: nticks
     integer :: curtick = 0
     real :: max_tick_time
 
-    character(len=*), parameter :: progress          = "/root/shared/results/progress"
+    character(len=*), parameter :: progress          = "/tmp/progress"
     character(len=*), parameter :: progress_template = "/root/shared/progress_template"
 
 contains
@@ -51,11 +52,14 @@ contains
             !$omp atomic
             curtick = curtick + 1
 
-            if (curtick < nticks) then
-                write (lun,*) tick_time(curtick)/max_tick_time
-            else
-                write (lun,*) 1.
-            end if
+            if (mod(curtick,THIN_OUTPUT)==0) then
+                if (curtick < nticks) then
+                    write (lun,*) tick_time(curtick)/max_tick_time
+                else
+                    write (lun,*) 1.
+                end if
+                flush(lun)
+            endif
         else
             call cpu_time(atime)
             write (lun,*) atime
